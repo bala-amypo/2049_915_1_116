d400d93ba63bda7b8c490e2812310fa6566b501e
bala-amypo <bala@amypo.in>
2025-12-19 08:51:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/Enity/Contract.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contract {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String contractNumber;
+
+    private String title;
+
+    private String counterpartyName;
+
+    private LocalDate agreedDeliveryDate;
+
+    private BigDecimal baseContractValue;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}

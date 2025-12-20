eae2c91e1980205c8ecda631edefa0a1d68898b4
bala-amypo <bala@amypo.in>
2025-12-20 07:45:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/BreachRuleService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.BreachRule;
+import java.util.List;
+
+public interface BreachRuleService {
+
+    BreachRule createRule(BreachRule rule);
+
+    BreachRule updateRule(Long id, BreachRule rule);
+
+    BreachRule getActiveDefaultOrFirst();
+
+    List<BreachRule> getAllRules();
+
+    void deactivateRule(Long id);
+}

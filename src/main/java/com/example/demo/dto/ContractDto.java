18ac4d2b3fd7a5cf54980f316364129cd342791e
bala-amypo <bala@amypo.in>
2025-12-20 08:58:54 +0000

    User commit

--- src/main/java/com/example/demo/dto/ContractDto.java
+++ src/main/java/com/example/demo/dto/ContractDto.java
@@ -1,73 +1,17 @@
 package com.example.demo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class ContractDto {
 
     private Long id;
     private String contractNumber;
     private String title;
     private String counterpartyName;
-    private BigDecimal baseContractValue;
-    private LocalDate agreedDeliveryDate;
-    private String status;
-
-    public ContractDto() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getContractNumber() {
-        return contractNumber;
-    }
-
-    public void setContractNumber(String contractNumber) {
-        this.contractNumber = contractNumber;
-    }
-
-    public String getTitle() {
-        return title;
-    }
- 
-    public void setTitle(String title) {
-        this.title = title;
-    }
- 
-    public String getCounterpartyName() {
-        return counterpartyName;
-    }
- 
-    public void setCounterpartyName(String counterpartyName) {
-        this.counterpartyName = counterpartyName;
-    }
-
-    public BigDecimal getBaseContractValue() {
-        return baseContractValue;
-    }
-
-    public void setBaseContractValue(BigDecimal baseContractValue) {
-        this.baseContractValue = baseContractValue;
-    }
-
-    public LocalDate getAgreedDeliveryDate() {
-        return agreedDeliveryDate;
-    }
-
-    public void setAgreedDeliveryDate(LocalDate agreedDeliveryDate) {
-        this.agreedDeliveryDate = agreedDeliveryDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
- 
-    public void setStatus(String status) {
-        this.status = status;
-    }
-}

0a7cf41a6f791ff92285dd8b5c75ffda5b385061
bala-amypo <bala@amypo.in>
2025-12-20 09:00:39 +0000

    User commit

--- src/main/java/com/example/demo/dto/BreachReportDto.java
+++ src/main/java/com/example/demo/dto/BreachReportDto.java
@@ -1,45 +1,19 @@
 package com.example.demo.dto;
 
-import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class BreachReportDto {
 
     private Long id;
     private Long contractId;
-    private int daysDelayed;
+    private Integer daysDelayed;
     private BigDecimal penaltyAmount;
-
-    public BreachReportDto() {}
-
-    public Long getId() {
-        return id;
-    }
- 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getContractId() {
-        return contractId;
-    }
- 
-    public void setContractId(Long contractId) {
-        this.contractId = contractId;
-    }
-
-    public int getDaysDelayed() {
-        return daysDelayed;
-    }
- 
-    public void setDaysDelayed(int daysDelayed) {
-        this.daysDelayed = daysDelayed;
-    }
-
-    public BigDecimal getPenaltyAmount() {
-        return penaltyAmount;
-    }
- 
-    public void setPenaltyAmount(BigDecimal penaltyAmount) {
-        this.penaltyAmount = penaltyAmount;
-    }
+    private String reportStatus;
+    private LocalDateTime generatedAt;
 }

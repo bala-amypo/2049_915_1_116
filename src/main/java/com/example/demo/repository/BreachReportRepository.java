8f2c29447822cd391a69e0eb174c70788568ad31
bala-amypo <bala@amypo.in>
2025-12-23 06:10:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/BreachReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.BreachReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BreachReportRepository extends JpaRepository<BreachReport, Long> {
-}
--- src/main/java/com/example/demo/repository/BreachRuleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.BreachRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BreachRuleRepository extends JpaRepository<BreachRule, Long> {
-}

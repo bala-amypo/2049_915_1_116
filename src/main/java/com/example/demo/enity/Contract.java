939de767e511e66f98f09a0624d71587a2a03e51
bala-amypo <bala@amypo.in>
2025-12-23 05:57:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/enity/Contract.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Contract {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String contractName;
+
+    private Double contractValue;
+
+    private String status;
+}

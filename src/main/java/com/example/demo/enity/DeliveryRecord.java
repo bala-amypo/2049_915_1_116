c040deb859c680449c8fac890471f5bfd284eb92
bala-amypo <bala@amypo.in>
2025-12-23 08:03:30 +0000

    User commit

--- src/main/java/com/example/demo/enity/DeliveryRecord.java
+++ src/main/java/com/example/demo/enity/DeliveryRecord.java
@@ -1,27 +1,17 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDate;
 
-@Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class DeliveryRecord {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Long contractId;
-
+    private int expectedQuantity;
+    private int deliveredQuantity;
     private LocalDate deliveryDate;
-
-    private Integer deliveredQuantity;
-
-    private Integer expectedQuantity;
 }
--- src/main/java/com/example/demo/enity/PenaltyCalculation.java
+++ src/main/java/com/example/demo/enity/PenaltyCalculation.java
@@ -1,25 +1,15 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
-@Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class PenaltyCalculation {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Long contractId;
-
-    private Double penaltyAmount;
-
-    private LocalDateTime calculatedAt;
+    private Long breachRuleId;
+    private double penaltyAmount;
 }

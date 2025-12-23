a8bb6a90321e4a850be9aae30e82922eb9d3c5ee
bala-amypo <bala@amypo.in>
2025-12-23 16:46:53 +0000

    User commit

--- src/main/java/com/example/demo/dto/BreachRuleDto.java
+++ src/main/java/com/example/demo/dto/BreachRuleDto.java
@@ -1,28 +1,2 @@
-package com.example.demo.entity;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.math.BigDecimal;
-
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class BreachRuleDto {   
 
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String ruleName;
-
-    private BigDecimal penaltyPerDay;
-
-    private boolean active;
-
-    private boolean defaultRule;
-}

5c6800fe2bd24cd5accb024b6ba17ca3ecb43b70
bala-amypo <bala@amypo.in>
2025-12-23 06:15:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/DeliveryRecordRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.DeliveryRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DeliveryRecordRepository extends JpaRepository<DeliveryRecord, Long> {
+
+    List<DeliveryRecord> findByContractId(Long contractId);
+
+    DeliveryRecord findTopByContractIdOrderByDeliveryDateDesc(Long contractId);
+}

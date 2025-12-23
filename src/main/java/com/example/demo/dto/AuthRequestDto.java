51341012c63e0f3078b323c38ab39898b48efe59
bala-amypo <bala@amypo.in>
2025-12-23 17:32:09 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequestDto.java
+++ src/main/java/com/example/demo/dto/AuthRequestDto.java
@@ -1,25 +1,30 @@
 package com.example.demo.dto;
 
-public class AuthRequest {
+public class AuthRequestDto {
 
-    private String email;
+    private String username;
     private String password;
 
-    public AuthRequest() {
+    public AuthRequestDto() {
     }
 
-    public String getEmail() {
-        return email;
+    public AuthRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
-    
+
     public void setPassword(String password) {
         this.password = password;
     }
--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class BadRequestAuthentication extends RuntimeException {
+public class BadRequestException extends RuntimeException {
 
-    public BadRequestAuthentication(String message) {
+    public BadRequestException(String message) {
         super(message);
     }
 }

6ea85e664c101afd4f959b79b84ae4c065f1a6db
bala-amypo <bala@amypo.in>
2025-12-24 19:10:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/BreachReport.java
+++ src/main/java/com/example/demo/entity/BreachReport.java
@@ -1,14 +1,23 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class BreachReport {
 
-    private Long contractId;
-    private int delayDays;
-    private double penaltyAmount;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String reportName;
+    private LocalDate breachDate;
+    private boolean resolved;
 }

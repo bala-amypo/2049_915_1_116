a8ff826bf325f7db6300020fa97623b81f1437c9
bala-amypo <bala@amypo.in>
2025-12-24 07:41:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ContractDto.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+public class ContractDto {
+    private String contractNumber;
+    private String title;
+    
+    public String getContractNumber() { return contractNumber; }
+    public void setContractNumber(String contractNumber) { this.contractNumber = contractNumber; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+}

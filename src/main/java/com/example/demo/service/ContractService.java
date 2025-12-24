9c1ec848d98ab0a1435188941c44060b79aa3241
bala-amypo <bala@amypo.in>
2025-12-24 07:59:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ContractService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Contract;
+import java.util.List;
+
+public interface ContractService {
+    Contract createContract(Contract contract);
+    Contract getContractById(Long id);
+    Contract updateContract(Long id, Contract contract);
+    List<Contract> getAllContracts();
+    void updateContractStatus(Long id);
+}

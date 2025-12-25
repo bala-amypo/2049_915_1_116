91b99f69e91c2f5b76d2561dca6792e6b0df933f
bala-amypo <bala@amypo.in>
2025-12-25 10:45:45 +0000

    User commit

--- src/main/java/com/example/demo/service/PenaltyCalculationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.PenaltyCalculation;
-
-public interface PenaltyCalculationService {
-
-    PenaltyCalculation calculatePenalty(Long contractId);
-}

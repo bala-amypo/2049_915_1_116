fa72f6ba8f0f1b1c40110d095727357bb61a13ab
bala-amypo <bala@amypo.in>
2025-12-25 11:06:46 +0000

    User commit

--- src/main/java/com/example/demo/dto/BreachReportDto.java
+++ src/main/java/com/example/demo/dto/BreachReportDto.java
@@ -1,13 +1,42 @@
 package com.example.demo.dto;
 
-import lombok.Data;
 import java.math.BigDecimal;
 
-@Data
-public class BreachRuleDto {
-    private String ruleName;
-    private BigDecimal penaltyPerDay;
-    private Double maxPenaltyPercentage;
-    private Boolean active;
-    private Boolean isDefaultRule;
+public class BreachReportDto {
+
+    private Long contractId;
+    private int daysDelayed;
+    private BigDecimal calculatedPenalty;
+
+    public BreachReportDto() {}
+
+    public BreachReportDto(Long contractId, int daysDelayed, BigDecimal calculatedPenalty) {
+        this.contractId = contractId;
+        this.daysDelayed = daysDelayed;
+        this.calculatedPenalty = calculatedPenalty;
+    }
+
+    public Long getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(Long contractId) {
+        this.contractId = contractId;
+    }
+
+    public int getDaysDelayed() {
+        return daysDelayed;
+    }
+
+    public void setDaysDelayed(int daysDelayed) {
+        this.daysDelayed = daysDelayed;
+    }
+
+    public BigDecimal getCalculatedPenalty() {
+        return calculatedPenalty;
+    }
+
+    public void setCalculatedPenalty(BigDecimal calculatedPenalty) {
+        this.calculatedPenalty = calculatedPenalty;
+    }
 }
